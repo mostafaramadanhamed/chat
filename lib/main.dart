71225ca606414db867d7455ad1db7a93e8efee6f
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-10-31 13:19:16 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:chat/features/landing/landin_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'common/constant/app_color.dart';
 import 'features/app_router.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
